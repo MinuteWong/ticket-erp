c18b520ad21ebb9c0d0551d38e83feafe5da3532
MinuteWang <243560619@qq.com>
2018-06-26 21:59:26 +0800

    爬取电影数据

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('koa-bodyparser')
 const serve = require('koa-static')
 const path = require('path')
 const cheerio = require('cheerio')
+const vm = require('vm')
 
 const app = new Koa()
 const router = new Router()
@@ -21,15 +22,11 @@ router.get('/', async (ctx, next) => {
   })
   const $ = cheerio.load(result)
   ctx.type = 'html'
-  const filmListEle = $('#dvfilm .l_c1_1')
-  //   console.log(filmListEle.find('li'))
-  const filmList = []
-  filmListEle.each(function () {
-    console.log($(this).find('a').length)
-    const film = $(this).find('a').text()
-    filmList.push(film)
-  })
-  ctx.body = `${filmListEle.html()}`
+  const filmListEle = $('head').find('script')
+  const sandbox = {}
+  vm.createContext(sandbox)
+  vm.runInContext(filmListEle.html(), sandbox)
+  ctx.body = sandbox.hotFilms
   next()
 })
 
