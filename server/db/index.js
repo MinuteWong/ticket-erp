98159a40d78624938b9fb38c10bc1bb86b00fdf9
minute <mingli.wang@zun1.com>
2018-06-27 18:31:52 +0800

    'dbconfig'

---
+++ server/db/index.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize')
+const config = require('../config/db_config')
+
+const sequelize = new Sequelize('ticketHack', config.user, config.password, {
+  host: config.host,
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+  // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+  operatorsAliases: false
+})
+
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.')
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err)
+//   })
+module.exports = sequelize
---
+++ server/models/CityModel.js
@@ -0,0 +1,3 @@
+const sequelize = require('../db')
+
+const City = sequelize.define('')
