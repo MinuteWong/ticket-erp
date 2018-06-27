de7a273f72dee46ffe4b877160ffe0a51206d5d6
MinuteWang <243560619@qq.com>
2018-06-27 22:22:17 +0800

    model ;

--- server/models/CityModel.js
+++ server/models/CityModel.js
@@ -1,5 +1,10 @@
 const sequelize = require('../db')
+const Sequelize = require('sequelize')
 
-const City = sequelize.define('city')
+const City = sequelize.define('city', {
+  breviary: Sequelize.STRING,
+  city_id: Sequelize.STRING,
+  city_name: Sequelize.STRING
+})
 
 module.exports = City
