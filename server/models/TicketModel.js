8abf6921b9bc44e4bb99eea121eea5d2d7047c2a
MinuteWang <243560619@qq.com>
2018-06-28 18:31:00 +0800

    '伪造数据'

--- server/models/TicketModel.js
+++ server/models/TicketModel.js
@@ -7,7 +7,7 @@ async function GetCity () {
     url: '/qryCitys.htm',
     method: 'post'
   })
-  const $ = cheerio.load(result)
+  const $ = cheerio.load(result.data)
   const cityDataEleList = $('#all_city_ul li')
   const cityData = []
   cityDataEleList.each(function () {
@@ -27,16 +27,26 @@ async function GetCity () {
 }
 
 async function GetHotFilms () {
+  const city = await request({
+    url: '/xhgCity.htm',
+    method: 'post',
+    data: {cityid: 110100},
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+      cookie: 'JSESSIONKEY=3c54416782734504b42e6bc8daa492ee; JSESSIONID=FACF2FB6BF4805DDA22CCDE2E752D6DE.tomcat2;'
+    }
+  })
   const result = await request({
     url: '/',
-    method: 'get'
+    method: 'get',
+    headers: {cookie: 'JSESSIONKEY=3c54416782734504b42e6bc8daa492ee; JSESSIONID=FACF2FB6BF4805DDA22CCDE2E752D6DE.tomcat2;'}
   })
-  const $ = cheerio.load(result)
+  const $ = cheerio.load(result.data)
   const filmListEle = $('head').find('script')
   const sandbox = {}
   vm.createContext(sandbox)
   vm.runInContext(filmListEle.html(), sandbox)
-  return sandbox
+  return $.html()
 }
 
 async function GetCinemaByFilmsId (id) {
@@ -65,7 +75,7 @@ async function GetSeats (cinemaid, theatersid) {
     url: `/seat/${cinemaid}/${theatersid}`,
     method: 'get'
   })
-  const $ = cheerio.load(result)
+  const $ = cheerio.load(result.data)
   const seats = $('.ch_seat')
   return seats
 }
@@ -77,3 +87,13 @@ module.exports = {
   GetCinemaByFilmsId,
   GetTheatersByCinemaId
 }
+
+// TXT_USER_CITY=440300;
+// Hm_lvt_f3a470502268bd255498a2e6bd23f8d4=1529976777,1529991242;
+// YXTID=4C53018B34D64FA3;
+// JSESSIONKEY=1ed8493963594fa9924a497f32ce9f8d;
+// JSESSIONID=CF8B4BEF35F1087DAD49AB91800FAEA1.tomcat1;
+// Hm_lpvt_f3a470502268bd255498a2e6bd23f8d4=1530174391
+
+// Set-Cookie: JSESSIONKEY=6e2ebc47e510435ab2d7b626e14d80cb; Path=/
+// Set-Cookie: JSESSIONID=0092E9F8AB4CCCCC201EDD5B4EF539CF.tomcat2; Path=/; HttpOnly
--- server/utils/request.js
+++ server/utils/request.js
@@ -1,10 +1,11 @@
 const axios = require('axios')
-
+const http = require('http')
 // const cookie =
 // 创建axios实例
 const service = axios.create({
   baseURL: 'http://www.189mv.cn/', // api的base_url
   timeout: 15000, // 请求超时时间
+  httpAgent: new http.Agent({ keepAlive: true }),
   validateStatus: function (status) {
     return [200, 201, 204, 422, 401, 400, 404, 429, 403].indexOf(status) !== -1 // 默认的
   }
@@ -12,7 +13,7 @@ const service = axios.create({
 
 service.interceptors.response.use(
   response => {
-    return response.data
+    return response
   }
 )
 
